397e6e7e7b354390f5973ee38eef6edfbf5fcbd0
Chaser1408 <tpsang.1408@gmail.com>
2024-11-03 13:35:07 +0700

    fix for vercel declare

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-        <nav>
-            <Link to="/">Home</Link>
-            |
-            <Link to="/about">About</Link>
-            |
-            <Link to="/project">Project</Link>
-            |
-            <Link to="/blog">Blog</Link>
-            |
-            <Link to="/contact">Contact</Link>
-        </nav>
+      <nav>
+        <Link to="/">Home</Link>
+        |
+        <Link to="/about">About</Link>
+        |
+        <Link to="/project">Project</Link>
+        |
+        <Link to="/blog">Blog</Link>
+        |
+        <Link to="/contact">Contact</Link>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
